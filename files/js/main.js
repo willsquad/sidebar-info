b3706a53211126e89a1b3f8990d7b9f22ca8f040
Ashil John <willsquad.aj@gmail.com>
2018-08-17 15:37:48 +0530

    July 17 Commit. Basic dynamic sidebar demo added

--- files/js/main.js
+++ files/js/main.js
@@ -87,27 +87,79 @@ $(document).ready(function(){
         $('#sidebar__content_'+id).addClass('active');
     });
 
+    /* Visibility Check */
+    $.fn.isFullyInViewport = function() {
+        var elementTop = $(this).offset().top;
+        var elementBottom = elementTop + $(this).outerHeight();
+        
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+        
+        return elementTop >= viewportTop && elementBottom <= viewportBottom;
+    };
 
-    $(document).on('scroll', function(){
+   /*  $.fn.isInViewport = function() {
+        var elementTop = $(this).offset().top;
+        var elementBottom = elementTop + $(this).outerHeight();
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+        return elementBottom > viewportTop && elementTop < viewportBottom;
+    }; */
+    /* Visibility Check */
+
+    $window = $(window);
+      
+    $window.on('resize scroll', function() {
+        $('.lhs_section_div').each(function() {
+
+            self = $(this);
+            var location = self.attr('data-id');
+            var heading = self.attr('data-h2');
+            var item_name = self.attr('data-item');
+            var item_image = self.attr('data-img');
+            var item_image = self.attr('data-img');
+            var about_text = self.attr('data-about');
+            var description_text = self.attr('data-description');
+            var data_more = self.attr('data-more');
+            var data_photos = self.attr('data-photos');
+
+            if ($(this).isFullyInViewport()) {
+                /* if(location == "about") {
+                    
+                } */
+                $('#category_name__h2').html(heading);
+                $('#sidebar__item_name__h3').html(item_name);
+                $('#sidebar__image').find('img').attr('src', item_image);
+                $('#about_text').html(about_text);
+                $('#description_text').html(description_text);
+
+                if(data_more == 1) {
+                    $('#more_details').show()
+                } else if(data_more == 0) {
+                    $('#more_details').hide()
+                }
+
+                if(data_photos == 1) {
+                    $('#photos_div').show()
+                } else if(data_photos == 0) {
+                    $('#photos_div').hide()
+                }
+            } 
+            /*if ($(this).isFullyInViewport()) {
+                console.log('Visible Div : '+location);
+            }  else {
+                console.log('Not Visible');
+            } */
+        });
+      });
+
+
+    /* $(document).on('scroll', function(){
         if($('#projects').visible(true)) {
             console.log('Projects');
         } else {
             console.log('Not found');
         }
-
-        /* if(location == 'intro') {
-                console.log('Intro in view');
-           } else if(location == 'about') {
-                console.log('About in view');
-           } else if(location == 'projects') {
-                console.log('Projects in view');
-           } else if(location == 'location') {
-                console.log('Location in view');
-           } else if(location == 'contact') {
-                console.log('Contact in view');
-           } else {
-               console.log('No divs in view.');
-           } */
-    });
+    }); */
     
 });
