b79c2ce6806f6281727c67012f9ad950b67b63ec
Ashil John <willsquad.aj@gmail.com>
2018-08-20 13:46:23 +0530

    v2 News article div base added, box shadow added, old js removed

--- files/js/main.js
+++ files/js/main.js
@@ -113,37 +113,11 @@ $(document).ready(function(){
         $('.lhs_section_div').each(function() {
 
             self = $(this);
-            var location = self.attr('data-id');
-            var heading = self.attr('data-h2');
-            var item_name = self.attr('data-item');
-            var item_image = self.attr('data-img');
-            var item_image = self.attr('data-img');
-            var about_text = self.attr('data-about');
-            var description_text = self.attr('data-description');
-            var data_more = self.attr('data-more');
-            var data_photos = self.attr('data-photos');
+
 
             if ($(this).isFullyInViewport()) {
-                /* if(location == "about") {
-                    
-                } */
-                $('#category_name__h2').html(heading);
-                $('#sidebar__item_name__h3').html(item_name);
-                $('#sidebar__image').find('img').attr('src', item_image);
-                $('#about_text').html(about_text);
-                $('#description_text').html(description_text);
 
-                if(data_more == 1) {
-                    $('#more_details').show()
-                } else if(data_more == 0) {
-                    $('#more_details').hide()
-                }
-
-                if(data_photos == 1) {
-                    $('#photos_div').show()
-                } else if(data_photos == 0) {
-                    $('#photos_div').hide()
-                }
+                
             } 
             /*if ($(this).isFullyInViewport()) {
                 console.log('Visible Div : '+location);
