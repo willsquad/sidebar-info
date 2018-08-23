3df5fab95d3dde8b522932b39266a3fb89e19839
Ashil John <willsquad.aj@gmail.com>
2018-08-23 15:29:36 +0530

    v3 RHS image and heading change on scroll

--- files/js/main.js
+++ files/js/main.js
@@ -48,42 +48,13 @@ $(document).ready(function(){
 
             $('.lhs_section_div').each(function() {
                 self = $(this);
-                var category = self.attr('data-category');
-                var title = self.attr('data-title');
-                var author = self.attr('data-author');
-                var authorimg = self.attr('data-authorimg');
-                var tags = self.attr('data-tags');
-                var date = self.attr('data-date');
-                var readtime = self.attr('data-readtime');
-                var images = self.attr('data-images');
-                var summary = self.attr('data-summary');
-                var main_img = self.attr('data-mainimg');
-                var link = self.attr('data-link');
+                var section = self.attr('data-id');
+                var main_img = self.attr('data-img');
+
     
-                var tags_split = tags.split(" ");
-                tags_output = '';
-                for(i=0; i<tags_split.length; i++) {
-                    tags_output += '<a href="'+tags_split[i].toLowerCase()+'">#'+tags_split[i]+'</a> ';
-                }
-    
-                var images_split = images.split(" ");
-                images_output = "";
-                for(i=0; i<images_split.length; i++){
-                    images_output += '<div class="image_div"><a href="'+images_split[i]+'" data-lightbox="news"><img src="'+images_split[i]+'" alt="News Images"></a></div>';
-                }
-    
-                if ($(this).isFullyInViewport() || $(this).isOnScreen()) {
-                    $('#category_name__h2').html(category);
-                    $('#sidebar__news_title').html(title);
+                if ($(this).isFullyInViewport()) {
+                    $('#sidebar__news_title').html(section);
                     $('#sidebar__image').find('img').attr('src', main_img);
-                    $('#author_name_container').find('img').attr('src', authorimg);
-                    $('#author_name_container').find('img').attr('title', author);
-                    $('#date_text').html(date);
-                    $('#read_time_text').html(readtime);
-                    $('#summary_text').html(summary);
-                    $('#go_to_article_button').attr('href', link);
-                    $('#tags_output_p').html(tags_output);
-                    $('#news_rhs_image_container').html(images_output);
                 } 
                 /*if ($(this).isFullyInViewport()) {
                     console.log('Visible Div : '+location);
