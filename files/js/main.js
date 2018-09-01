a4c2d862296c019577c3e9b787102d48ba7e6998
Ashil John <willsquad.aj@gmail.com>
2018-09-01 15:37:57 +0530

    v6 modified with horizontal cards

--- files/js/main.js
+++ files/js/main.js
@@ -83,7 +83,7 @@ $(document).ready(function(){
 
       }); */
       
-    /* $window.on('resize scroll', function() {
+    $window.on('resize scroll', function() {
 
         clearTimeout($.data(this, 'scrollTimer'));
         $.data(this, 'scrollTimer', setTimeout(function() {
@@ -98,6 +98,7 @@ $(document).ready(function(){
                 //var content_parse = eval('(' + content+ ')');
                 var content_parse = JSON.parse(content.replace(/'/g, '"'));
                 
+                
 
     
                 if ($(this).isFullyInViewport()) {
@@ -108,19 +109,56 @@ $(document).ready(function(){
                     } else {
                         $('#sidebar__image').hide();
                     }
-                    console.log(typeof(content_parse));
+
+                    console.log(content_parse);
+
                     var generated_content = '';
                     var i = 0;
                     for(var key in content_parse) {
+                        
+                        console.log(typeof(content_parse[key]))
 
                         i = ++i;
 
-                        generated_content += '<div class="content_div"><div class="img_text_container"><div class="card_img"><img src="files/images/'+i+'.jpeg" alt=""></div><div class="card_text"><div class="card_heading"><h5>'+key+'</h5></div><div class="card_description"><p>'+content_parse[key]+'</p></div></div><div class="card_expand_icon"><i class="fas fa-chevron-down"></i></div></div></div>';
+                        if(typeof(content_parse[key]) == 'string') {
+                            generated_content += '<div class="content_div"><div class="img_text_container"><div class="card_img"><img src="files/images/'+i+'.jpeg" alt=""></div><div class="card_text"><div class="card_heading"><h5>'+key+'</h5></div><div class="card_description"><p>'+content_parse[key]+'</p></div></div><div class="card_expand_icon"><i class="fas fa-chevron-down"></i></div></div></div>';
+                        } else if(typeof(content_parse[key]) == 'object') {
+                            i = 4;
+                            generated_content += '<div class="horizontal_card_container">';
+                            for(var horizontal_key in content_parse[key]) {
+                                i = ++i;
+                                //generated_content += horizontal_key+' : '+content_parse[key][horizontal_key];
+
+                                generated_content += '<div class="content_div h_card"> <div class="img_text_container"> <div class="card_img"><img src="files/images/'+i+'.jpeg" alt=""></div> <div class="card_text"> <div class="card_heading"> <h5>'+horizontal_key+'</h5> </div> <div class="card_description"> <p>'+content_parse[key][horizontal_key]+'</p> </div> </div> <div class="card_expand_icon"> <i class="fas fa-chevron-down"></i> </div> </div> </div>';
+                                
+                                
+                            
+                            }
+
+                            generated_content += '</div>';
+
+                            //$('.horizontal_card_container').mCustomScrollbar('update');
+
+                            
+                        }
+
+                        /* generated_content += '<div class="content_div"><div class="img_text_container"><div class="card_img"><img src="files/images/'+i+'.jpeg" alt=""></div><div class="card_text"><div class="card_heading"><h5>'+key+'</h5></div><div class="card_description"><p>'+content_parse[key]+'</p></div></div><div class="card_expand_icon"><i class="fas fa-chevron-down"></i></div></div></div>'; */
                     }
                     //console.log(generated_content);
-
+                    
 
                     $('#generated_content').html(generated_content);
+
+                    // For converting the dynamically loaded div to a scrollable div
+                    $(".horizontal_card_container").mCustomScrollbar({
+                        axis: "x",
+                        theme:"minimal-dark",
+                        advanced:{
+                            autoScrollOnFocus: false,
+                            updateOnContentResize: true
+                        }
+                    });
+                   
                     
                 } 
 
@@ -128,7 +166,9 @@ $(document).ready(function(){
 
         }, 250));
 
-      }); */
+        
+
+      });
 
 
     /* $(document).on('scroll', function(){
@@ -155,6 +195,12 @@ $(document).ready(function(){
         self.find('i').toggleClass('fa-chevron-up fa-chevron-down');
     });
 
+    $(document).on('click', '.h_card', function(){
+        var self = $(this);
+
+        
+    });
+
     
 
     $(".sidebar__content").mCustomScrollbar({
@@ -164,12 +210,12 @@ $(document).ready(function(){
     $(".horizontal_card_container").mCustomScrollbar({
         axis: "x",
         theme:"minimal-dark",
-        scrollInertia:0,
         advanced:{
             autoScrollOnFocus: false,
             updateOnContentResize: true
         }
     });
+
 
    /*  $("#sidebar__menu").mCustomScrollbar({
         axis: "x",
