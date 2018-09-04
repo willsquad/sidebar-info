18f71d7a03b4ac375b3d6d84fec3c0571df3da06
Ashil John <willsquad.aj@gmail.com>
2018-09-04 12:41:42 +0530

    v7 cards visible on load & basic scroll works (has positioning issue)

--- files/js/main.js
+++ files/js/main.js
@@ -209,19 +209,62 @@ $(document).ready(function(){
 
     }); */
 
+    generated_content = '';
+
+    $('.more_info').each(function() {
+        var self = $(this);
+        var keyword = self.html();
+
+        var content = self.data('info');
+        var content_parse = JSON.parse(content.replace(/'/g, '"'));
+
+        var div_id = keyword.toLowerCase().replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-');
+
+        var eTop = self.offset().top; //get the offset top of the element
+        var position_top = eTop - $(window).scrollTop() - 75;
+
+        //console.log(keyword);
+        //console.log(self.offset());
+        ///console.log($(window).scrollTop());
+
+        console.log(div_id+' : '+position_top);
+
+        
+
+        if(position_top >= 0) {
+            position_top = position_top;
+
+        } else if(position_top < 0) {
+            position_top = 0;
+        }
+        
+        
+        
+
+        generated_content += '<div class="content_div" id="'+div_id+'" style="position:relative; top:'+position_top+'px"><div class="img_text_container"><div class="card_img"><img src="'+content_parse.img+'" alt=""></div><div class="card_text"><div class="card_heading"><h5>'+keyword+'</h5></div><div class="card_description"><p>'+content_parse.text+'</p></div></div><div class="card_expand_icon"><i class="fas fa-chevron-down"></i></div></div></div>';
+
+
+        $('#generated_content').html(generated_content);
+    });
+
     $window.on('resize scroll', function() {
 
             $('.more_info').each(function() {
                 var self = $(this);
                 var keyword = self.html();
 
-                var content = self.data('info');
-                var content_parse = JSON.parse(content.replace(/'/g, '"'));
+                var div_id = keyword.toLowerCase().replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-');
+
+                //var content = self.data('info');
+                //var content_parse = JSON.parse(content.replace(/'/g, '"'));
 
                 //console.log(content_parse);
                 
                 var eTop = self.offset().top; //get the offset top of the element
                 var position_top = eTop - $(window).scrollTop() - 75;
+
+                //console.log(self.offset());
+                //console.log($(window).scrollTop());
 
                 /* if(position_top>0) { //visible
                     console.log(position_top);
@@ -229,13 +272,13 @@ $(document).ready(function(){
                 } else if(position_top<0) {
                     console.log('Keyword invisible');
                 } */
-                if(self.isFullyInViewport()) {
-                    var generated_content = '';
+                //if(self.isFullyInViewport()) {
+                    //var generated_content = '';
 
                     //console.log(content_parse.text);
                     //console.log(content_parse.img);
 
-                    console.log(position_top);
+                    // /console.log(position_top);
 
                     if(position_top >= 0) {
                         position_top = position_top;
@@ -244,17 +287,22 @@ $(document).ready(function(){
                         position_top = 0;
                     }
 
+                    //console.log(position_top);
+                    console.log(div_id+' : '+position_top);
+
+                    $('#'+div_id).css({'position':'relative','top':position_top+'px'});
+
 
                     //if(typeof(content_parse[key]) == 'string') {
-                        generated_content += '<div class="content_div" style="position:relative; top:'+position_top+'px"><div class="img_text_container"><div class="card_img"><img src="'+content_parse.img+'" alt=""></div><div class="card_text"><div class="card_heading"><h5>'+keyword+'</h5></div><div class="card_description"><p>'+content_parse.text+'</p></div></div><div class="card_expand_icon"><i class="fas fa-chevron-down"></i></div></div></div>';
+                        //generated_content += '<div class="content_div" id="'+div_id+'" style="position:relative; top:'+position_top+'px"><div class="img_text_container"><div class="card_img"><img src="'+content_parse.img+'" alt=""></div><div class="card_text"><div class="card_heading"><h5>'+keyword+'</h5></div><div class="card_description"><p>'+content_parse.text+'</p></div></div><div class="card_expand_icon"><i class="fas fa-chevron-down"></i></div></div></div>';
                     //}
 
 
-                    generated_content += '</div>';
+                    //generated_content += '</div>';
 
                     //console.log(generated_content);
-                    $('#generated_content').html(generated_content);
-                }
+                    //$('#generated_content').html(generated_content);
+                //}
                 
             });
 
@@ -296,4 +344,15 @@ $(document).ready(function(){
     });
     /* Custom Scrollbar */
     
+
+    function more_text() {
+        var more_html = '<div class="article_summary"><p>The state is wedged between the Lakshadweep Sea and the Western Ghats. Kerala experiences the humid equatorial tropic climate. The state has a coast of 590 km (370 mi)[108] and the width of the state varies between 11 and 121 kilometres (7 and 75 mi).[109] Geographically, Kerala can be divided into three climatically distinct regions: the eastern highlands; rugged and cool mountainous terrain, the central mid-lands; rolling hills, and the western lowlands; coastal plains.[110] Pre-Cambrian and Pleistocene geological formations compose the bulk of Kerala\'s terrain.[111][112] A catastrophic flood in Kerala in 1341 CE drastically modified its terrain and consequently affected its history; it also created a natural harbour for spice transport.[113] The eastern region of Kerala consists of high mountains, gorges and deep-cut valleys immediately west of the Western Ghats\' rain shadow.[110] 41 of Kerala\'s west-flowing rivers,[114] and 3 of its east-flowing ones originate in this region.[115][116] The Western Ghats form a wall of mountains interrupted only near Palakkad; hence also known Palghat, where the Palakkad Gap breaks.[117] The Western Ghats rise on average to 1,500 metres (4,900 feet) above sea level,[118] while the highest peaks reach around 2,500 metres (8,200 feet).[119] Anamudi in the Idukki district is the highest peak in south India, is at an elevation of 2,695 m (8,842 ft).</p></div>';
+        
+        $("#article_end").append(more_html);
+    }
+
+    more_text();
+    more_text();
+    more_text();
+    more_text();
 });
