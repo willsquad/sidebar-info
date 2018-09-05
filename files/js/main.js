7335d66784c7d0acf190515cf3014cd5230d775c
Ashil John <willsquad.aj@gmail.com>
2018-09-05 13:58:46 +0530

    v7 overlap issues fixed

--- files/js/main.js
+++ files/js/main.js
@@ -209,8 +209,9 @@ $(document).ready(function(){
 
     }); */
 
-    generated_content = ''; // Do not remove this. Otherwise, it will display '[object HTMLDivElement]' on adding content in addition to the content (https://linustechtips.com/main/topic/293402-object-htmldivelement/).
+    var generated_content = ''; // Do not remove this. Otherwise, it will display '[object HTMLDivElement]' on adding content in addition to the content (https://linustechtips.com/main/topic/293402-object-htmldivelement/).
 
+    var each_compare = [];
 
     $('.more_info').each(function(i) { //parameter i has been passed for counting 'each' element
         //$(this).data("serial", i);
@@ -227,11 +228,11 @@ $(document).ready(function(){
         var content = self.data('info');
         var content_parse = JSON.parse(content.replace(/'/g, '"'));
 
-        var div_id = keyword.toLowerCase().replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-'); // create a unique id based on the keyword, only alphanumeric characters
+        //var div_id = keyword.toLowerCase().replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-'); // create the unique id based on the keyword, only alphanumeric characters for matching with the ids created while LOADING the page [Removed: Will cause problems for keywords with same name. Replaced with "#content_i"]
 
         var eTop = self.offset().top; //get the offset top of the element
         var position_top = eTop - $(window).scrollTop() - 120; //120: 65px+15px+40px (65px:Height of the Article title section at the top of the Sidebar + 15px:Padding of the sidear content + 40px: half the height of the Card
-        position_top = position_top - (i * 115); //120: 80px+15px+15px+(5px [optional additional padding]) (80px: Height of each card + 15px:margin-top + 15px:margin-bottom)
+        position_top = position_top - (i * 115); //115px: (100px: Height of each card + 15px:margin-bottom)
 
         //console.log(keyword);
         //console.log(self.offset());
@@ -239,24 +240,31 @@ $(document).ready(function(){
 
         //console.log(div_id+' : '+position_top);
 
+        //console.log(position_top);
+        //each_compare.push(position_top);
+        //console.log(each_compare);
+
         
-        // If position is greater than 0, don't change it, else set it to 0
-        if(position_top >= 0) {
+        // If position is GREATER than or EQUAL TO 100px (single card height), don't change it, else set it to 0
+        if(position_top >= 100) {
             position_top = position_top;
 
-        } else if(position_top < 0) {
+        } else if(position_top < 100) {
             position_top = 0;
         }
         
         
         
+        
         // Cards generated on load
-        generated_content += '<div class="content_div" id="'+div_id+'" style="position:relative; top:'+position_top+'px"><div class="img_text_container"><div class="card_img"><img src="'+content_parse.img+'" alt=""></div><div class="card_text"><div class="card_heading"><h5>'+keyword+'</h5></div><div class="card_description"><p>'+content_parse.text+'</p></div></div><div class="card_expand_icon"><i class="fas fa-chevron-down"></i></div></div></div>';
+        generated_content += '<div class="content_div" id="content_'+i+'" data-serial="'+i+'" style="position:relative; top:'+position_top+'px"><div class="img_text_container"><div class="card_img"><img src="'+content_parse.img+'" alt=""></div><div class="card_text"><div class="card_heading"><h5>'+keyword+'</h5></div><div class="card_description"><p>'+content_parse.text+'</p></div></div><div class="card_expand_icon"><i class="fas fa-chevron-down"></i></div></div></div>';
 
         // Cards generated on load added to the sidebar
         $('#generated_content').html(generated_content);
 
     });
+
+    
 
     $window.on('resize scroll', function() {
 
@@ -268,7 +276,7 @@ $(document).ready(function(){
 
                 var keyword = self.html(); //get the keyaord
 
-                var div_id = keyword.toLowerCase().replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-'); // create the unique id based on the keyword, only alphanumeric characters for matching with the ids created while LOADING the page
+                //var div_id = keyword.toLowerCase().replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-'); // create the unique id based on the keyword, only alphanumeric characters for matching with the ids created while LOADING the page [Removed: Will cause problems for keywords with same name. Replaced with "#content_i"]
 
                 //var content = self.data('info');
                 //var content_parse = JSON.parse(content.replace(/'/g, '"'));
@@ -277,7 +285,7 @@ $(document).ready(function(){
                 
                 var eTop = self.offset().top; //get the offset top of the element
                 var position_top = eTop - $(window).scrollTop() - 120; //120: 65px+15px+40px (65px:Height of the Article title section at the top of the Sidebar + 15px:Padding of the sidear content + 40px: half the height of the Card
-                position_top = position_top - (i * 115); //120: 80px+15px+15px+(5px [optional additional padding]) (80px: Height of each card + 15px:margin-top + 15px:margin-bottom)
+                position_top = position_top - (i * 115); //115px: (100px: Height of each card + 15px:margin-bottom)
 
                 //console.log(self.offset());
                 //console.log($(window).scrollTop());
@@ -296,35 +304,46 @@ $(document).ready(function(){
 
                     // /console.log(position_top);
 
-                // If position is greater than 0, don't change it, else set it to 0
+                /* // If position is greater than 0, don't change it, else set it to 0
                 if(position_top >= 0) {
                     position_top = position_top;
         
                 } else if(position_top < 0) {
+                    position_top = 0;
+                } */
+
+                // If position is GREATER than or EQUAL TO 100px (single card height), don't change it, else set it to 0
+                if(position_top >= 100) {
+                    position_top = position_top;
+
+                } else if(position_top < 100) {
                     position_top = 0;
                 }
 
                 //console.log(position_top);
                 //console.log(div_id+' : '+position_top);
 
-                $('#'+div_id).css({'position':'relative','top':position_top+'px'});
-
-
-                    //if(typeof(content_parse[key]) == 'string') {
-                        //generated_content += '<div class="content_div" id="'+div_id+'" style="position:relative; top:'+position_top+'px"><div class="img_text_container"><div class="card_img"><img src="'+content_parse.img+'" alt=""></div><div class="card_text"><div class="card_heading"><h5>'+keyword+'</h5></div><div class="card_description"><p>'+content_parse.text+'</p></div></div><div class="card_expand_icon"><i class="fas fa-chevron-down"></i></div></div></div>';
-                    //}
-
-
-                    //generated_content += '</div>';
-
-                    //console.log(generated_content);
-                    //$('#generated_content').html(generated_content);
-                //}
-                
+                // Modify the position based on the ID of the RHS '.content_div's
+                $('#content_'+i).css({'position':'relative','top':position_top+'px'});
+                       
             });
 
-            
 
+
+
+            /*var content_div_count = 0;
+            
+            $('.content_div').each(function() {
+                if ($(this).isFullyInViewport()) {
+                   content_div_count++;
+                } 
+            });
+            
+            console.log(content_div_count);*/
+
+           /*  if(content_div_count > 6) {
+                $(".sidebar__content").mCustomScrollbar('scrollTo', '#content_1');
+            }  */
     });
 
 
