c0bfda39d3b6dab4c9696268b9cd2093df47342c
Ashil John <willsquad.aj@gmail.com>
2018-09-08 10:52:55 +0530

    v7 max width set

--- files/js/main.js
+++ files/js/main.js
@@ -1,5 +1,98 @@
 $(document).ready(function(){
 
+    /* Scrollspeed change */
+    jQuery.scrollSpeed = function(step, speed, easing) {
+        
+        var $document = $(document),
+            $window = $(window),
+            $body = $('html, body'),
+            option = easing || 'default',
+            root = 0,
+            scroll = false,
+            scrollY,
+            scrollX,
+            view;
+            
+        if (window.navigator.msPointerEnabled)
+        
+            return false;
+            
+        $window.on('mousewheel DOMMouseScroll', function(e) {
+            
+            var deltaY = e.originalEvent.wheelDeltaY,
+                detail = e.originalEvent.detail;
+                scrollY = $document.height() > $window.height();
+                scrollX = $document.width() > $window.width();
+                scroll = true;
+            
+            if (scrollY) {
+                
+                view = $window.height();
+                    
+                if (deltaY < 0 || detail > 0)
+            
+                    root = (root + view) >= $document.height() ? root : root += step;
+                
+                if (deltaY > 0 || detail < 0)
+            
+                    root = root <= 0 ? 0 : root -= step;
+                
+                $body.stop().animate({
+            
+                    scrollTop: root
+                
+                }, speed, option, function() {
+            
+                    scroll = false;
+                
+                });
+            }
+            
+            if (scrollX) {
+                
+                view = $window.width();
+                    
+                if (deltaY < 0 || detail > 0)
+            
+                    root = (root + view) >= $document.width() ? root : root += step;
+                
+                if (deltaY > 0 || detail < 0)
+            
+                    root = root <= 0 ? 0 : root -= step;
+                
+                $body.stop().animate({
+            
+                    scrollLeft: root
+                
+                }, speed, option, function() {
+            
+                    scroll = false;
+                
+                });
+            }
+            
+            return false;
+            
+        }).on('scroll', function() {
+            
+            if (scrollY && !scroll) root = $window.scrollTop();
+            if (scrollX && !scroll) root = $window.scrollLeft();
+            
+        }).on('resize', function() {
+            
+            if (scrollY && !scroll) view = $window.height();
+            if (scrollX && !scroll) view = $window.width();
+            
+        });       
+    };
+    
+    jQuery.easing.default = function (x,t,b,c,d) {
+    
+        return -c * ((t=t/d-1)*t*t*t - 1) + b;
+    };
+        
+    /* Scrollspeed change */
+
     /* Offscreen check */
     $.extend($.expr[':'], {
         'off-top': function(el) {
@@ -352,7 +445,9 @@ $(document).ready(function(){
                     //$('#content_'+i).fadeOut();
                 } else {
                     //$('#content_'+i).show();
+                    
                     $('#content_'+i).css({'position':'relative','top':position_top+'px'});
+                    
                     //$('#content_'+i).css({'display':'block'});
                     //$('#content_'+i).fadeIn();
                 }
@@ -495,6 +590,10 @@ $(document).ready(function(){
         }
     });
     /* Custom Scrollbar */
+
+    /* Change scrollspeed (step in px, duration in ms)*/
+    jQuery.scrollSpeed(50, 1000);
+    /* Change scrollspeed */
     
 
     function more_text() {
